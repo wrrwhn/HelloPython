385230a13aff474c098be5e072839be20c9fa4f7
wrrwhn <yqjdcyy@gmail.com>
2016-04-17 23:57:17 +0800

    gui

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # 参考资料
 
-- **[Python 简明教程](http://old.sebug.net/paper/python/)**
+- ~~**[Python 简明教程](http://old.sebug.net/paper/python/)**~~
 - [廖雪峰.python](http://www.liaoxuefeng.com/wiki/001374738125095c955c1e6d8bb493182103fac9270762a000)
 - [Python Documentation contents](https://docs.python.org/2/contents.html)
 - **[练手项目](https://www.zhihu.com/question/29372574)**
 - [Python 基础教程](http://www.runoob.com/python/python-tutorial.html)
 - [BeginnersGuideChinese](https://wiki.python.org/moin/BeginnersGuideChinese)
 
-# ~~[进度](https://wiki.python.org/moin/SimplePrograms)~~
+# [进度](https://wiki.python.org/moin/SimplePrograms)
--- feature/gui.py
+++ feature/gui.py
@@ -1,11 +1,27 @@
 
 # import
 # Tkinter
+	# http://www.runoob.com/python/python-gui-tkinter.html
+	# https://wiki.python.org/moin/TkInter
+	# https://docs.python.org/2/library/tkinter.html
 # wxPython
 # Jython
 
-import TKinter as tk
+import Tkinter as tk
 
-top = tk.Tk();
+# def
+class Application(tk.Frame):
+	def __init__(self, master=None):
+		tk.Frame.__init__(self, master)
+		self.grid()
+		self.createWidgets()
 
-top.mainloop()
+	def createWidgets(self):
+		self.quitButton= tk.Button(self, text='Quit', command=self.quit)
+		self.quitButton.grid()
+
+
+# use
+app= Application()
+app.master.title("Minimal Application")
+app.mainloop()
